21bc988b5e4bd796f76b7c5bd6861695135b222e
Oli Griffiths <oli.griffiths@me.com>
2016-04-25 20:00:43 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -56,18 +56,12 @@ node default {
   include dnsmasq
   include git
   include hub
-#  include nginx
+  include phpstorm
 
   # fail if FDE is not enabled
 #  if $::root_encrypted == 'no' {
 #    fail('Please enable full disk encryption and try again')
 #  }
-
-  # node versions
-  nodejs::version { '4.0.0': }
-
-  # default ruby versions
-  ruby::version { '2.2.4': }
 
   # common, useful packages
   package {
